98fe3b59ca2e7fe70eee6ab30b569fd994ac3fcf
Eric Balazy <ebalazy@unionbevnj.com>
2025-12-11 16:08:59 -0500

    Fix incorrect Context Provider hierarchy for SettingsProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { SettingsProvider } from './context/SettingsContext.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
 import { ErrorBoundary } from './components/ErrorBoundary.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ErrorBoundary>
-      <SettingsProvider>
-        <App />
-      </SettingsProvider>
+      <AuthProvider>
+        <SettingsProvider>
+          <App />
+        </SettingsProvider>
+      </AuthProvider>
     </ErrorBoundary>
   </StrictMode>,
 )
